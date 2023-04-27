d9ad0bcc3e7a832767f6565230f5da532d643f87
sreyes25 <sergreyes25@gmail.com>
2023-04-26 21:30:23 -0400

    Design pattern
    fixed

--- Design_Patterns/src/eager_initialization/Utilities.java
+++ Design_Patterns/src/eager_initialization/Utilities.java
@@ -1,17 +1,18 @@
 package eager_initialization;
 
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.ObjectOutputStream;
 
 public class Utilities {
-	public static void back(Bag bag) {
+	public static void back(Bag bag) throws IOException {
 		FileOutputStream fos = new FileOutputStream("data.txt");
 		ObjectOutputStream oos = new ObjectOutputStream(fos); 
 	}
 }
 
-public class Bag{
+
+ class Bag{
 	public void Bag() {
-		
 	}
-}
+	}
